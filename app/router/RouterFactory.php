3d03ae81c230f8e7ea6c99543f666df3fbec7b38
Čamaj Vladimír <vladimir.camaj@gmail.com>
2016-03-31 23:44:31 +0200

    Code style changed from tabs to 4 spaces.

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -3,11 +3,11 @@
 namespace App;
 
 
-use	Nette,
-	Nette\Application\Routers\RouteList,
-	Nette\Application\Routers\Route,
-	Nette\Application\Routers\SimpleRouter,
-	Nette\Utils\Strings;
+use    Nette;
+use    Nette\Application\Routers\RouteList;
+use    Nette\Application\Routers\Route;
+use    Nette\Application\Routers\SimpleRouter;
+use    Nette\Utils\Strings;
 
 
 /**
@@ -16,58 +16,58 @@ use	Nette,
 class RouterFactory
 {
 
-	/**
-	 * @return \Nette\Application\IRouter
-	 */
-	public static function createRouter()
-	{
-		$router = new RouteList();
+    /**
+     * @return \Nette\Application\IRouter
+     */
+    public static function createRouter()
+    {
+        $router = new RouteList();
 
-		$router[] = new Route('<presenter>[/<action>[/<id>]]',
-			array(
-				'presenter' => array(
-					Route::PATTERN => 'admin[^/]*|user|sign|helper|register|drom'
-				),
-			)
-		);
+        $router[] = new Route( '<presenter>[/<action>[/<id>]]',
+            array(
+                'presenter' => array(
+                    Route::PATTERN => 'admin[^/]*|user|sign|helper|register|drom',
+                ),
+            )
+        );
 
-		// Hranaté zátvorky sú povinné ak nepovinné parametre nenasledujú hneď po sebe
-		$router[] = new Route('[<presenter articles>/]<title>[/<action>/<vp-page \d+>]',
-			array(
-				'presenter' => array(
-					Route::VALUE => 'Articles',
-					Route::FILTER_TABLE => array(
-						'clanky' => 'Articles',
-					),
-				),
-				'action' => array(
-					Route::VALUE => 'show',
-					Route::FILTER_TABLE => array(
-						'strana' => 'show',
-					)
-				),
-				'title' => array(
-					Route::VALUE => 'najnovsie',
-				),
-				'vp-page' => array(
-					Route::VALUE => '1',
-				),
-			)
-		);
+        // Hranaté zátvorky sú povinné ak nepovinné parametre nenasledujú hneď po sebe
+        $router[] = new Route( '[<presenter articles>/]<title>[/<action>/<vp-page \d+>]',
+            array(
+                'presenter' => array(
+                    Route::VALUE        => 'Articles',
+                    Route::FILTER_TABLE => array(
+                        'clanky' => 'Articles',
+                    ),
+                ),
+                'action'    => array(
+                    Route::VALUE        => 'show',
+                    Route::FILTER_TABLE => array(
+                        'strana' => 'show',
+                    ),
+                ),
+                'title'     => array(
+                    Route::VALUE => 'najnovsie',
+                ),
+                'vp-page'   => array(
+                    Route::VALUE => '1',
+                ),
+            )
+        );
 
-		$router[] = new Route('<presenter>/<action>',
-			array(
-				'presenter' => array(
-					Route::VALUE => 'Default',
-				),
-				'action' => array(
-					Route::VALUE => 'default',
-				),
-			)
-		);
+        $router[] = new Route( '<presenter>/<action>',
+            array(
+                'presenter' => array(
+                    Route::VALUE => 'Default',
+                ),
+                'action'    => array(
+                    Route::VALUE => 'default',
+                ),
+            )
+        );
 
 
-		return $router;
-	}
+        return $router;
+    }
 
 }
